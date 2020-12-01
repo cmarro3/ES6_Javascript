2d4c817389a140e7583a3833dcaabc0ef356f064
cmarro3 <christophermarroquin352@gmail.com>
2020-11-30 19:31:35 -0600

    update

---
+++ ES6.js
@@ -0,0 +1,50 @@
+// Old Javascript
+
+/*var name = ['Eddy', 'Jhon', 'Sam'];
+
+var counter = 10;
+
+var counter = 5;
+
+console.log(counter);*/
+
+/*var counter = 5;
+function sayName(){
+    var name = "Chirs";
+    console.log(name);
+}
+sayName();*/
+
+//ES6
+
+// let & const
+const todolist = ["Milk", "Eggs"];
+
+//todolist = ["Milk"];
+
+/* Remember you can't change const at all unless you change it using
+* index notation or bracket notation. I have notes on how to change it.
+* */
+
+let counter = 10;
+
+// let counter = 5;
+/* you can't change let unless you write it as
+* counter = 5;
+* */
+console.log(counter);
+const list = [1,2,3,4,5];
+
+for( let i = 0; i < list.length; i++){
+    console.log(i);
+}
+// console.log(i)
+/* You can't console.log(i) outside of the function because
+* let and const are block scopes and can only be printed to
+* the console via within the function or what have you. */
+
+// Old Javascript
+
+//stopped on 8:05
+
+// ES6
